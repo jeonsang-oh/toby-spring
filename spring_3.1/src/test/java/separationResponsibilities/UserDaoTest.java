0147a999724b8f1d661ef60d322e60e1427278d5
jeonsang-oh <jeon.sangoh92@gmail.com>
2023-03-20 21:25:08 +0900

    1.3.3 - 관계설정 책임의 분리

---
+++ spring_3.1/src/test/java/separationResponsibilities/UserDaoTest.java
@@ -0,0 +1,33 @@
+package separationResponsibilities;
+
+import org.junit.jupiter.api.Test;
+import springbook.springbook_1_3.separationResponsibilities.user.dao.ConnectionMaker;
+import springbook.springbook_1_3.separationResponsibilities.user.dao.NConnectionMaker;
+import springbook.springbook_1_3.separationResponsibilities.user.dao.UserDao;
+import springbook.springbook_1_3.separationResponsibilities.user.domain.User;
+
+import java.sql.SQLException;
+
+public class UserDaoTest {
+    @Test
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        ConnectionMaker connectionMaker = new NConnectionMaker();
+
+        UserDao dao = new UserDao(connectionMaker);
+        User user = new User();
+
+        user.setId("whiteship");
+        user.setName("전상오");
+        user.setPassword("married");
+
+        dao.add(user);
+
+        System.out.println(user.getId() + " 등록 성공!");
+
+        User user2 = dao.get(user.getId());
+        System.out.println(user2.getName());
+        System.out.println(user2.getPassword());
+
+        System.out.println(user2.getId() + " 조회 성공!");
+    }
+}
